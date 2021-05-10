763a1334313407a842b97526a56a6498ec012378
Gabriel Bernardo Martins <gabrielb3rnardo@gmail.com>
2021-05-10 10:21:30 -0300

    Fim do app

--- lib/main.dart
+++ lib/main.dart
@@ -18,25 +18,46 @@ class Home extends StatefulWidget {
 }
 
 class _HomeState extends State<Home> {
-
   final _toDoController = TextEditingController();
 
   List _toDoList = [];
+  Map<String, dynamic> _lastRemoved;
+  int _lastRemovedPos;
 
-  /*@override
-  void initState(){
+  @override
+  void initState() {
     super.initState();
-    _readData().then(data);
-  }*/
+    _readData().then((data) {
+      setState(() {
+        _toDoList = json.decode(data);
+      });
+    });
+  }
 
-  void _addToDo(){
-  setState(() {
-    Map<String, dynamic> newToDo = Map();
-    newToDo["title"] = _toDoController.text;
-    _toDoController.text ="";
-    newToDo["ok"] =  false;
-    _toDoList.add(newToDo);
-  });
+  void _addToDo() {
+    setState(() {
+      Map<String, dynamic> newToDo = Map();
+      newToDo["title"] = _toDoController.text;
+      _toDoController.text = "";
+      newToDo["ok"] = false;
+      _toDoList.add(newToDo);
+      _savedata();
+    });
+  }
+
+  Future<Null> _refresh() async {
+    await Future.delayed(Duration(seconds: 1));
+    setState(() {
+      _toDoList.sort((a, b) {
+        if (a["ok"] && !b["ok"])
+          return 1;
+        else if (!a["ok"] && b["ok"])
+          return -1;
+        else
+          return 0;
+      });
+      _savedata();
+    });
 
   }
 
@@ -70,26 +91,69 @@ class _HomeState extends State<Home> {
             ),
           ),
           Expanded(
+            child: RefreshIndicator(
+              onRefresh: _refresh,
               child: ListView.builder(
-                  padding: EdgeInsets.only(top: 10),
-                  itemCount: _toDoList.length,
-
-                  itemBuilder: (context, index) {
-                    return CheckboxListTile(
-                      title: Text(_toDoList[index]["title"]),
-                      value: _toDoList[index]["ok"],
-                      secondary: CircleAvatar(
-                        child: Icon(_toDoList[index]["ok"] ? Icons.check : Icons.error),
-                      ),
-                      onChanged: (c){
-                       setState(() {
-                         _toDoList[index]["ok"] = c;
-                       });
-                      },
-                    );
-                  }))
+                padding: EdgeInsets.only(top: 10),
+                itemCount: _toDoList.length,
+                itemBuilder: buildItem,
+              ),
+            ),
+          ),
         ],
       ),
+    );
+  }
+
+  Widget buildItem(context, index) {
+    return Dismissible(
+      key: Key(DateTime.now().millisecondsSinceEpoch.toString()),
+      background: Container(
+        color: Colors.red,
+        child: Align(
+          alignment: Alignment(-0.9, 0.0),
+          child: Icon(
+            Icons.delete,
+            color: Colors.white,
+          ),
+        ),
+      ),
+      direction: DismissDirection.startToEnd,
+      child: CheckboxListTile(
+        title: Text(_toDoList[index]["title"]),
+        value: _toDoList[index]["ok"],
+        secondary: CircleAvatar(
+          child: Icon(_toDoList[index]["ok"] ? Icons.check : Icons.error),
+        ),
+        onChanged: (c) {
+          setState(() {
+            _toDoList[index]["ok"] = c;
+            _savedata();
+          });
+        },
+      ),
+      onDismissed: (direction) {
+        setState(() {
+          _lastRemoved = Map.from(_toDoList[index]);
+          _lastRemovedPos = index;
+          _toDoList.removeAt(index);
+          _savedata();
+
+          final snack = SnackBar(
+            content: Text("Tarefa ${_lastRemoved["title"]}\" Removida!"),
+            action: SnackBarAction(
+                label: "Desfazer",
+                onPressed: () {
+                  setState(() {
+                    _toDoList.insert(_lastRemovedPos, _lastRemoved);
+                    _savedata();
+                  });
+                }),
+            duration: Duration(seconds: 2),
+          );
+          ScaffoldMessenger.of(context).showSnackBar(snack);
+        });
+      },
     );
   }
 
